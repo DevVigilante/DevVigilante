be59421d7bb2880d3b4e48bbb9a257532986c65d
DevVigilante <96680248+DevVigilante@users.noreply.github.com>
2025-10-17 18:14:57 +0530

    refactor: make title, phrases and social links as constants

--- src/app/page.js
+++ src/app/page.js
@@ -2,29 +2,35 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./header.js"
 
+const title = "Dev Kumar";
+
+const myPhrases = [
+    "Android Developer",
+    "Java | Shell | Gradle knowledge",
+    "Self taught",
+    "Front end website designer",
+    "Adapting new programming languages",
+    "Open Source project Developer"
+];
+
+const socialLinks = [
+  {
+    link: "https://www.instagram.com/innovative_cst?igsh=MWQ0bDFsbGJ0NmpobQ==",
+    text: "Instagram"
+  },
+  {
+    link: "https://www.github.com/DevVigilante",
+    text: "GitHub"
+  }
+];
+
 export default function Home() {
   return (
     <div>
       <Header
-      title="Dev Kumar"
-      phrases = {[
-        "Android Developer",
-        "Java | Shell | Gradle knowledge",
-        "Self taught",
-        "Front end website designer",
-        "Adapting new programming languages",
-        "Open Source project Developer"
-      ]}
-      links={[
-        {
-          link: "https://www.instagram.com/innovative_cst?igsh=MWQ0bDFsbGJ0NmpobQ==",
-          text: "Instagram"
-        },
-        {
-          link: "https://www.github.com/DevVigilante",
-          text: "GitHub"
-        }
-      ]}
+      title = {title}
+      phrases = {myPhrases}
+      links = {socialLinks}
       />
     </div>
   );
