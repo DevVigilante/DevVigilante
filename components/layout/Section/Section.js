287a54cd202fa7d2522f3f737a84ffaab75b2171
DevVigilante <96680248+DevVigilante@users.noreply.github.com>
2025-10-19 11:22:24 +0530

    ui: make ui responsive

--- components/layout/Section/Section.js
+++ components/layout/Section/Section.js
@@ -4,8 +4,8 @@ import styles from "./Section.module.css";
 export default function Section({title, content, className}) {
   return (
     <section className={`${styles.section} ${className}`}>
-      <h2>{title}</h2>
-      <div>{content}</div>
+      <h2 className={styles.heading}>{title}</h2>
+      <div className={styles.para}>{content}</div>
     </section>
   );
 }
